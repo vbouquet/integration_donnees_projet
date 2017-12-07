8e1e299f2ed417feb50d37effb9088008bde5697
remy <remypoc@free.fr>
2017-12-07 12:39:23 +0100

    Fix Mr Bouquet errors

--- src/main/java/fr/parisnanterre/idd/model/BDD.java
+++ src/main/java/fr/parisnanterre/idd/model/BDD.java
@@ -3,11 +3,11 @@ package fr.parisnanterre.idd.model;
 import java.util.List;
 
 public class BDD {
-    public List<Cours> cours;
-    public List<Enseignant> enseignants;
-    public List<Enseigne> enseignes;
-    public List<Etudiant> etudiants;
-    public List<Inscription> inscriptions;
+    private List<Cours> cours;
+    private List<Enseignant> enseignants;
+    private List<Enseigne> enseignes;
+    private List<Etudiant> etudiants;
+    private List<Inscription> inscriptions;
 
     public List<Cours> getCours() {
         return cours;
