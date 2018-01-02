305b2f859ce8bb02bf7d21ac107e504eaca670f2
Remy Pocquerusse <remypoc@free.fr>
2018-01-02 19:27:28 +0100

    Use set instead of list

--- src/main/java/fr/parisnanterre/idd/model/BDD.java
+++ src/main/java/fr/parisnanterre/idd/model/BDD.java
@@ -1,60 +1,90 @@
 package fr.parisnanterre.idd.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BDD {
-    private List<Cours> cours;
-    private List<Enseignant> enseignants;
-    private List<Enseigne> enseignes;
-    private List<Etudiant> etudiants;
-    private List<Inscription> inscriptions;
+    private Set<Cours> cours;
+    private Set<Enseignant> enseignants;
+    private Set<Enseigne> enseignes;
+    private Set<Etudiant> etudiants;
+    private Set<Inscription> inscriptions;
 
     public BDD() {
-        cours = new ArrayList<>();
-        enseignants = new ArrayList<>();
-        enseignes = new ArrayList<>();
-        etudiants = new ArrayList<>();
-        inscriptions = new ArrayList<>();
+        cours = new LinkedHashSet<>();
+        enseignants = new LinkedHashSet<>();
+        enseignes = new LinkedHashSet<>();
+        etudiants = new LinkedHashSet<>();
+        inscriptions = new LinkedHashSet<>();
     }
 
-    public List<Cours> getCours() {
+    public Set<Cours> getCours() {
         return cours;
     }
 
-    public void setCours(List<Cours> cours) {
+    public void setCours(Set<Cours> cours) {
         this.cours = cours;
     }
 
-    public List<Enseignant> getEnseignants() {
+    public Set<Enseignant> getEnseignants() {
         return enseignants;
     }
 
-    public void setEnseignants(List<Enseignant> enseignants) {
+    public void setEnseignants(Set<Enseignant> enseignants) {
         this.enseignants = enseignants;
     }
 
-    public List<Enseigne> getEnseignes() {
+    public Set<Enseigne> getEnseignes() {
         return enseignes;
     }
 
-    public void setEnseignes(List<Enseigne> enseignes) {
+    public void setEnseignes(Set<Enseigne> enseignes) {
         this.enseignes = enseignes;
     }
 
-    public List<Etudiant> getEtudiants() {
+    public Set<Etudiant> getEtudiants() {
         return etudiants;
     }
 
-    public void setEtudiants(List<Etudiant> etudiants) {
+    public void setEtudiants(Set<Etudiant> etudiants) {
         this.etudiants = etudiants;
     }
 
-    public List<Inscription> getInscriptions() {
+    public Set<Inscription> getInscriptions() {
         return inscriptions;
     }
 
-    public void setInscriptions(List<Inscription> inscriptions) {
+    public void setInscriptions(Set<Inscription> inscriptions) {
         this.inscriptions = inscriptions;
+    }
+
+    public void addCours(Cours cours) {
+        this.cours.add(cours);
+    }
+
+    public void addEnseignant(Enseignant enseignant) {
+        this.enseignants.add(enseignant);
+    }
+
+    public void addEnseigne(Enseigne enseigne) {
+        this.enseignes.add(enseigne);
+    }
+
+    public void addEtudiant(Etudiant etudiant) {
+        this.etudiants.add(etudiant);
+    }
+
+    public void addInscription(Inscription inscription) {
+        this.inscriptions.add(inscription);
+    }
+
+    @Override
+    public String toString() {
+        return "BDD{" +
+                "cours=" + cours +
+                ", enseignants=" + enseignants +
+                ", enseignes=" + enseignes +
+                ", etudiants=" + etudiants +
+                ", inscriptions=" + inscriptions +
+                '}';
     }
 }
